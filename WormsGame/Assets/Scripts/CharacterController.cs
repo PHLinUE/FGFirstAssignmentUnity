03140eb2176561e729f6624b93a2b46fc66501d7
PHLinUE <philip.lindholm0207@gmail.com>
2022-09-26 15:16:59 +0200

    aaaa

---
+++ WormsGame/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private PlayerTurn playerTurn;
+    [SerializeField] private Rigidbody characterBody;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float rotationSpeed = 15f;
+
+    void Update()
+    {
+        if (playerTurn.IsPlayerTurn())
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                transform.Rotate(transform.up * rotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+            }
+
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                transform.Translate(transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"), Space.World);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && IsTouchingFloor())
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        //characterBody.velocity = Vector3.up * 10f;
+        characterBody.AddForce(Vector3.up * 500f);
+    }
+
+    private bool IsTouchingFloor()
+    {
+        RaycastHit hit;
+        // Parameters:
+        // - The center from where we shoot
+        // - Radius of the sphere
+        // - Direction of the sphere
+        // - hit parameter
+        // - Distance the sphere
+        bool result =  Physics.SphereCast(transform.position, 0.15f, -transform.up, out hit, 1f);
+        return result;
+    }
+}

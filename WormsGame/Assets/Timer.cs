7edef1a615e33fabaa95748dacd49dcdb79c43c7
PHLinUE <philip.lindholm0207@gmail.com>
2022-09-26 12:13:03 +0200

    TimerScript 1.0 and basic environment, colors for different players

---
+++ WormsGame/Assets/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float currenTime = 0f;
+    float startingTime = 10f;
+
+    void Start()
+    {
+        currenTime = startingTime;
+    }
+
+    void Update()
+    {
+        currenTime -= 1 * Time.deltaTime;
+        print (currenTime);
+    }
+
+}
